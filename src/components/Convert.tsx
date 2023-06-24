30eef003fca5e0b2bf1d479d8cc919773ac0c68d
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-24 19:45:43 -0300

    fix: mobile size adjustments

--- src/components/Convert.tsx
+++ src/components/Convert.tsx
@@ -16,22 +16,22 @@ export const Convert = () => {
                     </div>
                     <div className='w-full lg:pl-28 font-medium overflow-hidden'>
                         <div className='mb-4'>
-                            <h3 className='text-3xl lg:text-[43px] lg:leading-[48px] max-w-[600px]'>Pronto para triplicar sua <strong>Geração de Leads?</strong></h3>
-                            <p className='text-lg lg:text-2xl mt-2 lg:mt-4'>Criação e ativação em <strong>4 minutos.</strong></p>
+                            <h3 className='text-[24px] leading-8 lg:text-[43px] lg:leading-[48px] max-w-[600px] mt-5 md:mt-0'>Pronto para triplicar sua <strong>Geração de Leads?</strong></h3>
+                            <p className='text-[16px] lg:text-2xl mt-2 lg:mt-4'>Criação e ativação em <strong>4 minutos.</strong></p>
                         </div>
                         <hr className='border border-gray-300' />
-                        <div className='mt-5 flex gap-4'>
-                            <button className='uppercase text-sm lg:text-[1.1rem] font-bold text-white lg:py-4 px-6 py-2 lg:px-8 bg-primary rounded-full'>Ver demonstração</button>
-                            <Image className='w-[40%] lg:w-[205px] lg:h-[73px]' src={rdStation} alt='Selo RD-Station' />
+                        <div className='mt-5 flex gap-2 2xl:gap-4'>
+                            <button className='w-[50%] uppercase text-xs 2xl:text-[1.1rem] font-semibold text-white lg:py-4 px-6 py-2 lg:px-8 bg-primary rounded-full'>Ver demonstração</button>
+                            <Image className='md:w-[40%] 2xl:w-[205px] lg:h-[73px]' src={rdStation} alt='Selo RD-Station' />
                         </div>
-                        <div className='flex flex-col lg:flex-row mt-4 font-semibold gap-4 lg:gap-0'>
-                            <div className='flex items-center gap-1 lg:pr-2 lg:border-r-2 border-gray-400'>
+                        <div className='flex flex-col 2xl:flex-row mt-4 font-semibold gap-4 2xl:gap-0'>
+                            <div className='flex items-center gap-1 2xl:pr-3 2xl:border-r-2 border-gray-400'>
                                 <Image className='lg:w-[16px]' src={noCard} alt='Selo RD-Station' />
-                                <p><strong>Não </strong>é necessário Cartão de Crédito</p>
+                                <p className='text-[15px] md:text-md'><strong>Não </strong>é necessário Cartão de Crédito</p>
                             </div>
-                            <div className='flex items-center gap-1 lg:pl-2'>
+                            <div className='flex items-center gap-1 2xl:pl-3'>
                                 <Image className='lg:w-[92px]' src={rating} alt='Selo RD-Station' />
-                                <p><strong>4.9</strong>/5 média de satisfação</p>
+                                <p className='text-[14px] md:text-md'><strong>4.9</strong>/5 média de satisfação</p>
                             </div>
                         </div>
                     </div>
