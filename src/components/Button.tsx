4b5a0e2cb44462a65cd8d5dfd542fe2d16c7bbd6
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-26 18:55:14 -0300

    refact: buttons being rendered with their current state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type iBtn = {
     content: string
+    isActive: boolean
 }
 
-export const Button = ({ content }: iBtn) => {
+export const Button = ({ content, isActive }: iBtn) => {
+
+    const [active, setActive] = useState('bg-primary text-white border-primary')
 
     return (
         <button
-            className="py-1 px-5 xl:py-2 xl:px-6 rounded-full text-sm md:text-md border border-gray-800 font-semibold hover:border-primary hover:text-primary duration-500">
+            className={`${isActive == true ? active : ''} py-1 px-5 xl:py-2 xl:px-6 rounded-full text-sm md:text-md border border-gray-800 font-semibold hover:border-primary duration-500`}
+        >
             {content}
         </button>
     )
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 import { Button } from './Button'
 import { Dropdown } from './Dropdown'
 
+const filterButtons = [
+    { content: 'Agências', isActive: false },
+    { content: 'Chatbot', isActive: false },
+    { content: 'Marketing Digital', isActive: true },
+    { content: 'Geração de Leads', isActive: false },
+    { content: 'Mídia Paga', isActive: false }
+]
+
 export const Filters = () => {
+
+    const [filters, setFilters] = useState(filterButtons)
+
     return (
         <div className='flex flex-wrap justify-center items-center lg:justify-between mb-6 lg:mb-0'>
             <div className='flex justify-center flex-wrap gap-2'>
-                <Button content="Agências" />
-                <Button content="Chatbot" />
-                <Button content="Marketing Digital" />
-                <Button content="Geração de Leads" />
-                <Button content="Mídia Paga" />
+                {
+                    filters.map((btn, index) => (
+                        <Button key={index} content={btn.content} isActive={btn.isActive} />
+                    ))
+                }
             </div>
             <div className="mt-6 lg:mt-2 xl:mt-0">
                 <Dropdown />
