fe2511d645a25e3c1d0818fd1b736b83b927424e
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-27 14:26:06 -0300

    feat: lib framer motion added, and created the first animation in the title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,37 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
 import heroImg from "/public/images/asset-header.png";
+import { motion } from "framer-motion"
 
 export const Hero = () => {
+
+    const title = {
+        hidden: { scale: 0 },
+        show: { scale: 1 }
+    }
+
     return (
         <div className='bg-secondary h-[58vh] flex justify-center items-center'>
             <div className='text-center'>
                 <span className='uppercase text-primary text-[11px] md:text-[14px] font-extrabold border-2 border-primary py-1.5 px-4 rounded-full rounded-es-none'>Webinars exclusivos</span>
                 <div className='flex flex-col items-center'>
                     <h2 className='capitalize font-medium text-xl md:text-3xl lg:text-5xl mt-6'>Menos conversinha,</h2>
-                    <h1 className='relative capitalize font-bold linear-gradient text-4xl md:text-5xl lg:text-7xl xl:text-8xl pb-6'>
+                    <motion.h1
+                        className='relative capitalize font-bold linear-gradient text-4xl md:text-5xl lg:text-7xl xl:text-8xl pb-6'
+                        variants={title}
+                        initial='hidden'
+                        animate='show'
+                        transition={{ type: 'spring', damping: 3.5, mass: 0.2, stiffness: 150 }}
+                    >
                         Mais conversão
                         <Image
                             src={heroImg}
                             alt="detail"
                             className="absolute top-0 right-[-10px] lg:right-[-15px] w-6 md:w-7 lg:w-10 xl:top-2 2xl:w-[48px]"
                         />
-                    </h1>
+                    </motion.h1>
                     <hr className='bg-gray-300 h-[2px] w-[50vw] mx-auto' />
 
                 </div>
