f5a4eab6892bf38061302fbff51096fe3c5601fd
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-27 15:59:16 -0300

    fix: button size on hover in Convert component

--- src/components/Convert.tsx
+++ src/components/Convert.tsx
@@ -21,7 +21,7 @@ export const Convert = () => {
                         </div>
                         <hr className='border border-gray-300' />
                         <div className='justify-center lg:justify-start mt-5 flex flex-wrap md:flex-nowrap gap-y-5 gap-2 2xl:gap-4'>
-                            <button className='w-[80%] lg:w-[45%] uppercase text-xs 2xl:text-[1.1rem] font-semibold text-white py-5 px-6 lg:px-8 bg-primary rounded-full hover:bg-white hover:text-primary hover:border hover:border-primary duration-500'>Ver demonstração</button>
+                            <button className='w-[80%] lg:w-[45%] uppercase text-xs 2xl:text-[1.1rem] font-semibold text-white py-5 px-6 lg:px-8 bg-primary border border-primary rounded-full hover:bg-white hover:text-primary hover:border-primary duration-500'>Ver demonstração</button>
                             <Image className='w-[50%] 2xl:w-[205px] lg:h-[73px]' src={rdStation} alt='Selo RD-Station' />
                         </div>
                         <div className='flex flex-col 2xl:flex-row mt-4 font-semibold gap-4 2xl:gap-0'>
